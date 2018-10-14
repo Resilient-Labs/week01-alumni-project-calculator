f27730e54f8c5dc2a9da83f4fdd1b72251eae24e
Karina Dorantes <danirak21@gmail.com>
2018-10-13 20:42:06 -0400

    styling calculator & buttons

--- calcCode/main.js
+++ calcCode/main.js
@@ -2,10 +2,11 @@ window.addEventListener('load', function(){
   // console.log("this works");
   var calcBtns = ["0","1","2","3","4","5","6","7","8","9","/","x","+","-","=","."];
   calcBtns.forEach(function(el){
+    var calculator = document.getElementById("calculator");
     var btn = document.createElement("a")
     btn.setAttribute("href", "#");
     btn.innerHTML = el;
     btn.className = "btn";
-    document.body.appendChild(btn);
+    calculator.appendChild(btn);
   })
 })
